97ea3b1c5eae9998e034e070f2ad685a4bbbe92d
Valentin <baluionut22@gmail.com>
2022-04-22 20:07:52 +0300

    round2

--- CrowdfundingLib/Project.cs
+++ CrowdfundingLib/Project.cs
@@ -11,21 +11,41 @@ namespace CrowdfundingLib
         public string Title { get; set; }
         public string Description { get; set; }
         public float CurrentAmount { get; set; }
+        public float GoalAmount { get; set; }
+        public int Transactions { get; set; }
         public DateTimeOffset Deadline { get; set; }
 
         public Project(string title,
                        string description,
-                       DateTimeOffset deadline)
+                       DateTimeOffset deadline, float GAmount)
         {
             Title = title;
             Description = description;
             Deadline = deadline;
+            GoalAmount = GAmount;
             CurrentAmount = 0;
+            Transactions = 0;
         }
 
         public void Fund(float amount)
         {
+            if (amount < 0)
+            {
+                throw new ArgumentException("Cannot fund with negative amount");
+            }
+            
+            if(CurrentAmount >= GoalAmount)
+            {
+                throw new OverflowException("Cannot fund this project, because it has already reached its goal amount of funds!");
+            }
+
             CurrentAmount += amount;
+            Transactions++;
+
+            if(CurrentAmount >= GoalAmount)
+            {
+                Console.WriteLine("The project has reached the goal amount of funds!");
+            }
         }
 
         public override bool Equals(object? obj)
@@ -33,12 +53,13 @@ namespace CrowdfundingLib
             return obj is Project project &&
                    Title == project.Title &&
                    Description == project.Description &&
-                   Deadline.Equals(project.Deadline);
+                   Deadline.Equals(project.Deadline) &&
+                   GoalAmount == project.GoalAmount;
         }
 
         public override int GetHashCode()
         {
-            return HashCode.Combine(Title, Description, Deadline);
+            return HashCode.Combine(Title, Description, Deadline, GoalAmount);
         }
     }
 }
--- CrowdfundingTest/UnitTest.cs
+++ CrowdfundingTest/UnitTest.cs
@@ -1,6 +1,7 @@
 using CrowdfundingLib;
 using NUnit.Framework;
 using System;
+using System.IO;
 
 namespace CrowdfundingTest
 {
@@ -16,7 +17,7 @@ namespace CrowdfundingTest
         [Test]
         public void AddProject_ValidProject_PresentInPlatform()
         {
-            Project project = new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(7));
+            Project project = new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(7), 10000);
             Platform.AddProject(project);
             Assert.That(Platform.Projects, Has.Exactly(1).EqualTo(project));
         }
@@ -24,7 +25,7 @@ namespace CrowdfundingTest
         [Test]
         public void AddProject_PastDeadline_ThrowsArgumentException()
         {
-            Project project = new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(-7));
+            Project project = new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(-7), 10000);
             
             Assert.That(
                 () => Platform.AddProject(project),
@@ -36,7 +37,7 @@ namespace CrowdfundingTest
         public void FundProject_PositiveAmount_CurrentAmountUpdated()
         {
             // Arrange
-            Project createdProject = Platform.AddProject(new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(7)));
+            Project createdProject = Platform.AddProject(new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(7), 10000));
             float amountToFund = 200;
 
             // Act
@@ -50,17 +51,84 @@ namespace CrowdfundingTest
         public void FundProject_NegativeAmount_ThrowsArgumentException()
         {
             // Arrange
-            Project createdProject = Platform.AddProject(new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(7)));
+            Project createdProject = Platform.AddProject(new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(7), 10000));
             float amountToFund = -200;
 
-            // Act
-            createdProject.Fund(amountToFund);
+            // Act & Assert
 
             Assert.That(
-                () => Platform.AddProject(createdProject),
+                () => createdProject.Fund(amountToFund),
                 Throws.InstanceOf<ArgumentException>()
                         .With.Message.EqualTo("Cannot fund with negative amount"));
         }
+
+        [Test]
+        public void FundProject_GoalAmountReached_DisplayMessage()
+        {
+            //Arrange
+            Project newProject = Platform.AddProject(new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(5), 540));
+
+            float firstAmount = 100;
+            float secondAmount = 440;
+
+            var stringWriter = new StringWriter();
+            Console.SetOut(stringWriter);
+
+            //Act
+            newProject.Fund(firstAmount);
+            newProject.Fund(secondAmount);
+
+            //Assert 
+
+            Assert.AreEqual("The project has reached the goal amount of funds!\r\n", stringWriter.ToString());           
+        }
+
+        [Test]
+        public void FundProject_GoalAmountOverfulfilled_ThrowsOverflowException()
+        {
+            //Arrange
+            Project newProject = Platform.AddProject(new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(9), 810));
+
+            float firstAmount = 300;
+            float secondAmount = 510;
+            float overflowAmount = 100;
+
+            //Act
+            newProject.Fund(firstAmount);
+            newProject.Fund(secondAmount);
+
+            //Assert 
+            Assert.That(
+                () => newProject.Fund(overflowAmount),
+                Throws.InstanceOf<OverflowException>()
+                        .With.Message.EqualTo("Cannot fund this project, because it has already reached its goal amount of funds!"));
+        }
+
+        [Test]
+        public void FundProject_TransactionNumber_EqualsExpected()
+        {
+            //Arrange
+            Project newProject = Platform.AddProject(new Project("foo", "bar", DateTimeOffset.UtcNow.AddDays(12), 1010));
+
+            float firstAmount = 300;
+            float secondAmount = 510.27f;
+            float thirdAmount = 90.80f;
+            float fourthAmount = 10.9f;
+            float fifthAmount = 50.5f;
+
+            int expectedTransactionsNumber = 5;
+
+            //Act
+            newProject.Fund(firstAmount);
+            newProject.Fund(secondAmount);
+            newProject.Fund(thirdAmount);
+            newProject.Fund(fourthAmount);
+            newProject.Fund(fifthAmount);
+
+            //Assert 
+            Assert.AreEqual(expectedTransactionsNumber, newProject.Transactions);
+        }
+
 
     }
 }
